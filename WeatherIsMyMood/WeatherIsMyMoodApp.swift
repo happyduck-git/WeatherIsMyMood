34766aa456569b0131317ad8f212c6feb58905e7
GG <rkrudtls@gmail.com>
2023-12-09 10:46:54 +0900

    Initial Commit

---
+++ WeatherIsMyMood/WeatherIsMyMoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherIsMyMoodApp.swift
+//  WeatherIsMyMood
+//
+//  Created by HappyDuck on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherIsMyMoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
