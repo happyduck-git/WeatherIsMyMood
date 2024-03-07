2ef3ab86ce92c01b76dc5ffda2af9353e880759b
GG <rkrudtls@gmail.com>
2024-03-07 15:19:34 +0900

    Amend shell script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,12 +6,12 @@
 #  Created by HappyDuck on 3/7/24.
 #  
 
-FOLDER_PATH="Volumes/workspace/repository"
-CONFIG_FILE_NAME="Config.xcconfig"
-CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILE_NAME"
-
-# Add environment variables to xcconfig.
-echo "OPEN_WEATHER_API_KEY = $OPEN_WEATHER_API_KEY" >> "$CONFIG_FILE_PATH"
-
-cat "$CONFIG_FILE_PATH"
+#FOLDER_PATH="Volumes/workspace/repository"
+#CONFIG_FILE_NAME="Config.xcconfig"
+#CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILE_NAME"
+#
+## Add environment variables to xcconfig.
+#echo "OPEN_WEATHER_API_KEY = $OPEN_WEATHER_API_KEY" >> "$CONFIG_FILE_PATH"
+#
+#cat "$CONFIG_FILE_PATH"
 
