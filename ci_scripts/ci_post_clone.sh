e9456e4377dde553ae14901b549405d80bdcc564
GG <rkrudtls@gmail.com>
2024-03-07 15:36:38 +0900

    Amend shell script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,7 +6,7 @@
 #  Created by HappyDuck on 3/7/24.
 #  
 
-FOLDER_PATH="Volumes/workspace/repository"
+FOLDER_PATH="/Volumes/workspace/repository"
 CONFIG_FILE_NAME="Config.xcconfig"
 CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILE_NAME"
 
